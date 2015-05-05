a4f71b5c352ed600830000ee8f77dc2f446311cf
Daniel Bouman <d.bouman1@gmail.com>
2015-05-05 18:57:30 +0200

    improved wavefunction initialization and adjusted potential for 2D, something goes wrong with the potential

--- QD/dynamics.py
+++ QD/dynamics.py
@@ -8,7 +8,6 @@ from anim2D import animate_wavefunction
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
 
-
 class Particle:
   
   def __init__(self,a,L_x,L_y,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y):
@@ -19,25 +18,21 @@ class Particle:
     self.yAxis = np.linspace(0,L_y,L_y/a)
     
     # Define the momentum operator matrices
-    self.H_x = np.multiply(sp.eye(L_x/a,k=0),-2)
-    self.H_x = self.H_x + sp.eye(L_x/a,k=-1)
-    self.H_x = self.H_x + sp.eye(L_x/a,k=1)
-    self.H_x = np.divide(self.H_x,-a**2).todense()
-    
-    self.H_y = np.multiply(sp.eye(L_y/a,k=0),-2)
-    self.H_y = self.H_y + sp.eye(L_y/a,k=-1)
-    self.H_y = self.H_y + sp.eye(L_y/a,k=1)
-    self.H_y = np.divide(self.H_y,-a**2).todense()
+    self.H_x = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(L_x/a,L_x/a)),-a**2).todense()
+    self.H_y = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(L_y/a,L_y/a)),-a**2).todense()
 
     # Wave function
     self.psi_x = np.multiply(np.exp((-1/sigma_x)*np.power(self.xAxis-mu_x,2)),np.exp(-1j*k_x*self.xAxis))
     self.psi_y = np.multiply(np.exp((-1/sigma_y)*np.power(self.yAxis-mu_y,2)),np.exp(-1j*k_y*self.yAxis))
     
-
-  def potential(self,pos,amp):
+  def potential(self,xStart,xEnd,yStart,yEnd,amp):
     # Add potential to hamiltonian matrix
-    H_index = pos/self.a
-    self.H_x[H_index,H_index] = self.H_x[H_index,H_index] + amp
+    xIndexStart = xStart/self.a
+    xIndexEnd = xEnd/self.a
+    yIndexStart = yStart/self.a
+    yIndexEnd = yEnd/self.a
+    self.H_x[xIndexStart:xIndexEnd,xIndexStart:xIndexEnd] = self.H_x[xIndexStart:xIndexEnd,xIndexStart:xIndexEnd] + amp
+    self.H_y[yIndexStart:yIndexEnd,yIndexStart:yIndexEnd] = self.H_y[yIndexStart:yIndexEnd,yIndexStart:yIndexEnd] + amp
 
   def normalize_wavefunction(self):
     inner_product = self.a*sum(np.multiply(self.psi_x,np.conj(self.psi_x)))*self.a*sum(np.multiply(self.psi_y,np.conj(self.psi_y)))
@@ -56,9 +51,9 @@ class Particle:
 
     # Time is run here
     for i in range(0,duration):
+        # Solve linear equations for each dimension
         self.time_evolved_psi_x[:,i],_ = linalg.bicgstab(A_x,B_x.dot(self.psi_x).transpose())
         self.psi_x = self.time_evolved_psi_x[:,i]
-
         self.time_evolved_psi_y[:,i],_ = linalg.bicgstab(A_y,B_y.dot(self.psi_y).transpose())
         self.psi_y = self.time_evolved_psi_y[:,i]
 
@@ -66,7 +61,6 @@ class Particle:
     time_evolved_probability_x = np.real(np.multiply(self.time_evolved_psi_x,np.conj(self.time_evolved_psi_x)))
     time_evolved_probability_y = np.real(np.multiply(self.time_evolved_psi_y,np.conj(self.time_evolved_psi_y)))
     time_evolved_probability = np.zeros((self.L_x/self.a,self.L_y/self.a,self.duration),dtype=float)
-
 
     for i in range(0,self.duration):
         time_evolved_probability[:,:,i] = np.outer(time_evolved_probability_x[:,i],time_evolved_probability_y[:,i])
@@ -76,12 +70,10 @@ class Particle:
     # animate_wavefunction(time_evolved_probability,self.L,self.a,self.duration)
     # plt.plot(self.xAxis,sel)
 
-
-
     x_mesh,y_mesh = np.meshgrid(self.xAxis,self.yAxis)
     
     # fig = plt.figure()
-    # ax = plt.axes(xlim=(0, self.L_x), ylim=(0, self.L_y))  
+    # ax = plt.axes(xlim=(0, self.L_x), ylim=(0, self.L_y))
     # plt.xlabel(r'x')
     # plt.ylabel(r'y')
 
--- main.py
+++ main.py
@@ -2,28 +2,30 @@
 from QD.dynamics import Particle
 
 # Parameters
-a = 0.25       # Spatial resolution
-L_x = 40       # Domain size
-L_y = 40 
-sigma_x = 2.5     # Wavefunction shape
-sigma_y = 6.5
-k_x = 1/300         # Wave vector
-k_y = -1/7         # Wave vector
-mu_x = 10
-mu_y = 30
-hbar = 1      # Reduced Planck constant
+a = 0.25        # Spatial resolution
+L_x = 40        # Domain length in x-direction
+L_y = 40        # Domain lenght in y-direction
+sigma_x = 2.5   # Wavefunction shape x-direction
+sigma_y = 2.5   # Wavefunction shape y-direction
+mu_x = 5        # Wavefunction position x-direction
+mu_y = 20       # Wavefunction position y-direction
+k_x = 99999     # Wave vector x-direction
+k_y = 0         # Wave vector y-direction
+hbar = 1        # Reduced Planck constant
 # Potential
-pos = 0      # Potential position
-amp = 0      # Potential height
+xStart =  20
+xEnd = 21
+yStart = 0
+yEnd = 40
+amp = 0         # Potential height
 
 # Time evolution
 tau = 0.1
-duration = 500
-
+duration = 100
 
 particle1 = Particle(a,L_x,L_y,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y)     # Initialize particle
 particle1.normalize_wavefunction()      # normalize wavefunction so that probability sums up to unity
-particle1.potential(pos,amp)          # Initialize potential
+particle1.potential(xStart,yEnd,yStart,yEnd,amp)          # Initialize potential
 particle1.timeEvolution(tau,hbar,duration)             # Start time evolution of particle
 particle1.plot2D()
 # particle1.plot()
