f855ab63fbfff39c004e6f49f9f91e578dbd8c68
Daniel Bouman <d.bouman1@gmail.com>
2015-05-06 12:41:20 +0200

    fixed 2d

--- QD/dynamics.py
+++ QD/dynamics.py
@@ -1,29 +1,32 @@
 import numpy as np
+from numpy import linalg as LA
 import scipy.sparse as sp
 import scipy.sparse.linalg as linalg
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import time
-from anim2D import animate_wavefunction
-import pylab
-from mpl_toolkits.mplot3d import Axes3D
+import sys
+# from anim2D import animate_wavefunction
+# import time
+# import pylab
+# from mpl_toolkits.mplot3d import Axes3D
 
 class Particle:
   
   def __init__(self,a,L_x,L_y,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y):
-    self.L_x = L_x # box length
-    self.L_y = L_y # box length
+    self.sizeX = int(np.ceil(L_x/a)) # box length
+    self.sizeY = int(np.ceil(L_y/a)) # box length
     self.a = a # spatial resolution
     self.xAxis = np.linspace(0,L_x,L_x/a)
     self.yAxis = np.linspace(0,L_y,L_y/a)
     
-    # Define the momentum operator matrices
-    self.H_x = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(L_x/a,L_x/a)),-a**2).todense()
-    self.H_y = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(L_y/a,L_y/a)),-a**2).todense()
+    # Define the momentum part of the Hamiltonian matrices
+    self.H_x = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(self.sizeX,self.sizeX)),-a**2).todense()
+    self.H_y = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(self.sizeY,self.sizeY)),-a**2).todense()
 
     # Wave function
-    self.psi_x = np.multiply(np.exp((-1/sigma_x)*np.power(self.xAxis-mu_x,2)),np.exp(-1j*k_x*self.xAxis))
-    self.psi_y = np.multiply(np.exp((-1/sigma_y)*np.power(self.yAxis-mu_y,2)),np.exp(-1j*k_y*self.yAxis))
+    psi_x = np.multiply(np.exp((-1/sigma_x)*np.power(self.xAxis-mu_x,2)),np.exp(-1j*k_x*self.xAxis))
+    psi_y = np.multiply(np.exp((-1/sigma_y)*np.power(self.yAxis-mu_y,2)),np.exp(-1j*k_y*self.yAxis))
+    self.psi = np.outer(psi_x,psi_y)
     
   def potential(self,xStart,xEnd,yStart,yEnd,amp):
     # Add potential to hamiltonian matrix
@@ -35,35 +38,39 @@ class Particle:
     self.H_y[yIndexStart:yIndexEnd,yIndexStart:yIndexEnd] = self.H_y[yIndexStart:yIndexEnd,yIndexStart:yIndexEnd] + amp
 
   def normalize_wavefunction(self):
-    inner_product = self.a*sum(np.multiply(self.psi_x,np.conj(self.psi_x)))*self.a*sum(np.multiply(self.psi_y,np.conj(self.psi_y)))
-    self.psi_x = (1/np.sqrt(np.abs(inner_product)))*self.psi_x
+    self.psi = (1/np.linalg.norm(self.psi))*self.psi
     
-  def timeEvolution(self,tau,hbar,duration):
+  def timeEvolution(self,tau,duration):
     self.duration = duration
     # Define A and B matrices
-    A_x = sp.identity(self.L_x/self.a) - tau/(1j*hbar)*self.H_x
-    B_x = sp.identity(self.L_x/self.a) + tau/(1j*hbar)*self.H_x
-    self.time_evolved_psi_x = np.zeros((self.L_x/self.a,duration),dtype=complex)
+    A_x = sp.identity(self.sizeX) - tau/(1j)*self.H_x
+    B_x = sp.identity(self.sizeX) + tau/(1j)*self.H_x
+    # self.time_evolved_psi_x = np.zeros((self.sizeX,duration),dtype=complex)
 
-    A_y = sp.identity(self.L_y/self.a) - tau/(1j*hbar)*self.H_y
-    B_y = sp.identity(self.L_y/self.a) + tau/(1j*hbar)*self.H_y
-    self.time_evolved_psi_y = np.zeros((self.L_x/self.a,duration),dtype=complex)
+    A_y = sp.identity(self.sizeY) - tau/(1j)*self.H_y
+    B_y = sp.identity(self.sizeY) + tau/(1j)*self.H_y
+    self.time_evolved_psi = np.zeros((self.sizeX,self.sizeY,duration),dtype=complex)
 
-    # Time is run here
+    # Start time evolution of particle
     for i in range(0,duration):
-        # Solve linear equations for each dimension
-        self.time_evolved_psi_x[:,i],_ = linalg.bicgstab(A_x,B_x.dot(self.psi_x).transpose())
-        self.psi_x = self.time_evolved_psi_x[:,i]
-        self.time_evolved_psi_y[:,i],_ = linalg.bicgstab(A_y,B_y.dot(self.psi_y).transpose())
-        self.psi_y = self.time_evolved_psi_y[:,i]
+      print(i)
+      # sys.stdout.write("Timestep: %d of %d \r" % (i+1,duration) )
+      for ii in range(0,self.sizeY):
+        # Solve linear equation Crank Nicelson
+        # x-direction
+        self.time_evolved_psi[:,ii,i],_ = linalg.bicgstab(A_x,B_x.dot(self.psi[:,ii]).transpose())
+        self.psi[:,ii] = self.time_evolved_psi[:,ii,i]
+        # y-direction
+        self.time_evolved_psi[:,ii,i],_ = linalg.bicgstab(A_y,B_y.dot(self.psi[:,ii]).transpose())
+        self.psi[:,ii] = self.time_evolved_psi[:,ii,i]
 
   def plot2D(self):
-    time_evolved_probability_x = np.real(np.multiply(self.time_evolved_psi_x,np.conj(self.time_evolved_psi_x)))
-    time_evolved_probability_y = np.real(np.multiply(self.time_evolved_psi_y,np.conj(self.time_evolved_psi_y)))
-    time_evolved_probability = np.zeros((self.L_x/self.a,self.L_y/self.a,self.duration),dtype=float)
+    time_evolved_probability = np.real(np.multiply(self.time_evolved_psi,np.conj(self.time_evolved_psi)))
+    # time_evolved_probability_y = np.real(np.multiply(self.time_evolved_psi_y,np.conj(self.time_evolved_psi_y)))
+    # time_evolved_probabilit y= np.zeros((self.sizeX,self.sizeY,self.duration),dtype=float)
 
-    for i in range(0,self.duration):
-        time_evolved_probability[:,:,i] = np.outer(time_evolved_probability_x[:,i],time_evolved_probability_y[:,i])
+    # for i in range(0,self.duration):
+        # time_evolved_probability[:,:,i] = np.outer(time_evolved_probability_x[:,i],time_evolved_probability_y[:,i])
 
     # print(time_evolved_probability[:,:,0])
 
--- main.py
+++ main.py
@@ -11,13 +11,12 @@ mu_x = 5        # Wavefunction position x-direction
 mu_y = 20       # Wavefunction position y-direction
 k_x = 99999     # Wave vector x-direction
 k_y = 0         # Wave vector y-direction
-hbar = 1        # Reduced Planck constant
 # Potential
 xStart =  20
 xEnd = 21
 yStart = 0
 yEnd = 40
-amp = 0         # Potential height
+amp = 1         # Potential height
 
 # Time evolution
 tau = 0.1
@@ -26,6 +25,6 @@ duration = 100
 particle1 = Particle(a,L_x,L_y,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y)     # Initialize particle
 particle1.normalize_wavefunction()      # normalize wavefunction so that probability sums up to unity
 particle1.potential(xStart,yEnd,yStart,yEnd,amp)          # Initialize potential
-particle1.timeEvolution(tau,hbar,duration)             # Start time evolution of particle
+particle1.timeEvolution(tau,duration)             # Start time evolution of particle
 particle1.plot2D()
 # particle1.plot()
