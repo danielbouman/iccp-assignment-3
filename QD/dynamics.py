49d92250431487d1d89513e25f09d7e64f350844
Daniel Bouman <d.bouman1@gmail.com>
2015-05-06 17:11:37 +0200

    implemented correct 2D CNM, y-direction doesnt decay as quickly as x-dir

--- QD/dynamics.py
+++ QD/dynamics.py
@@ -3,39 +3,42 @@ from numpy import linalg as LA
 import scipy.sparse as sp
 import scipy.sparse.linalg as linalg
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import sys
+# import matplotlib.animation as animation
+# import sys
 # from anim2D import animate_wavefunction
 # import time
 # import pylab
 # from mpl_toolkits.mplot3d import Axes3D
 
-class Particle:
+class CrankNicolson:
   
-  def __init__(self,a,L_x,L_y,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y):
-    self.sizeX = int(np.ceil(L_x/a)) # box length
-    self.sizeY = int(np.ceil(L_y/a)) # box length
+  def __init__(self,a,L,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y):
+    self.gridLength = int(L/a) # box length
     self.a = a # spatial resolution
-    self.xAxis = np.linspace(0,L_x,L_x/a)
-    self.yAxis = np.linspace(0,L_y,L_y/a)
+    self.xAxis = np.linspace(0,L,self.gridLength)
+    self.yAxis = np.linspace(0,L,self.gridLength)
     
     # Define the momentum part of the Hamiltonian matrices
-    self.H_x = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(self.sizeX,self.sizeX)),-a**2).todense()
-    self.H_y = np.divide(sp.diags([1,-2,1],[-1,0,1], shape=(self.sizeY,self.sizeY)),-a**2).todense()
-
+    c = 1/(a**2)
+    xNeighbors = sp.diags([c,-2*c,c],[-1,0,1],shape=(self.gridLength,self.gridLength))
+    self.H = sp.kron(sp.eye(self.gridLength),xNeighbors) + sp.diags([1,1],[-self.gridLength,self.gridLength],shape=(self.gridLength**2,self.gridLength**2))
+    
     # Wave function
     psi_x = np.multiply(np.exp((-1/sigma_x)*np.power(self.xAxis-mu_x,2)),np.exp(-1j*k_x*self.xAxis))
     psi_y = np.multiply(np.exp((-1/sigma_y)*np.power(self.yAxis-mu_y,2)),np.exp(-1j*k_y*self.yAxis))
-    self.psi = np.outer(psi_x,psi_y)
+    self.psi = np.outer(psi_x,psi_y).flatten()
     
-  def potential(self,xStart,xEnd,yStart,yEnd,amp):
+  def potential(self,function):
+    if function == "wall":
+      V = sp.csr_matrix((3,3),dtype="float")
+      # V[] =
+    
     # Add potential to hamiltonian matrix
-    xIndexStart = xStart/self.a
-    xIndexEnd = xEnd/self.a
-    yIndexStart = yStart/self.a
-    yIndexEnd = yEnd/self.a
-    self.H_x[xIndexStart:xIndexEnd,xIndexStart:xIndexEnd] = self.H_x[xIndexStart:xIndexEnd,xIndexStart:xIndexEnd] + amp
-    self.H_y[yIndexStart:yIndexEnd,yIndexStart:yIndexEnd] = self.H_y[yIndexStart:yIndexEnd,yIndexStart:yIndexEnd] + amp
+    # xIndexStart = int(xStart/self.a)
+    # xIndexEnd = int(xEnd/self.a)
+    # yIndexStart = int(yStart/self.a)
+    # yIndexEnd = int(yEnd/self.a)
+    # self.H_x[xIndexStart:xIndexEnd,xIndexStart:xIndexEnd] = self.H_x[xIndexStart:xIndexEnd,xIndexStart:xIndexEnd] + amp
 
   def normalize_wavefunction(self):
     self.psi = (1/np.linalg.norm(self.psi))*self.psi
@@ -43,29 +46,20 @@ class Particle:
   def timeEvolution(self,tau,duration):
     self.duration = duration
     # Define A and B matrices
-    A_x = sp.identity(self.sizeX) - tau/(1j)*self.H_x
-    B_x = sp.identity(self.sizeX) + tau/(1j)*self.H_x
-    # self.time_evolved_psi_x = np.zeros((self.sizeX,duration),dtype=complex)
-
-    A_y = sp.identity(self.sizeY) - tau/(1j)*self.H_y
-    B_y = sp.identity(self.sizeY) + tau/(1j)*self.H_y
-    self.time_evolved_psi = np.zeros((self.sizeX,self.sizeY,duration),dtype=complex)
+    A = sp.identity(self.gridLength**2) - tau/(1j)*self.H
+    B = sp.identity(self.gridLength**2) + tau/(1j)*self.H
+    
+    self.time_evolved_psi = np.zeros((self.gridLength**2,duration),dtype=complex)
 
     # Start time evolution of particle
     for i in range(0,duration):
       print(i)
-      # sys.stdout.write("Timestep: %d of %d \r" % (i+1,duration) )
-      for ii in range(0,self.sizeY):
-        # Solve linear equation Crank Nicelson
-        # x-direction
-        self.time_evolved_psi[:,ii,i],_ = linalg.bicgstab(A_x,B_x.dot(self.psi[:,ii]).transpose())
-        self.psi[:,ii] = self.time_evolved_psi[:,ii,i]
-        # y-direction
-        self.time_evolved_psi[:,ii,i],_ = linalg.bicgstab(A_y,B_y.dot(self.psi[:,ii]).transpose())
-        self.psi[:,ii] = self.time_evolved_psi[:,ii,i]
+      # Solve linear equation A*psi(t + tau) = B*psi(t)
+      self.time_evolved_psi[:,i],_ = linalg.bicgstab(A,B.dot(self.psi).transpose())
+      self.psi = self.time_evolved_psi[:,i]
 
   def plot2D(self):
-    time_evolved_probability = np.real(np.multiply(self.time_evolved_psi,np.conj(self.time_evolved_psi)))
+    self.time_evolved_probability = np.real(np.multiply(self.time_evolved_psi,np.conj(self.time_evolved_psi))).reshape(self.gridLength,self.gridLength,self.duration)
     # time_evolved_probability_y = np.real(np.multiply(self.time_evolved_psi_y,np.conj(self.time_evolved_psi_y)))
     # time_evolved_probabilit y= np.zeros((self.sizeX,self.sizeY,self.duration),dtype=float)
 
--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 # ICCP assignment 3
-from QD.dynamics import Particle
+from QD.dynamics import CrankNicolson
 
 # Parameters
-a = 0.25        # Spatial resolution
-L_x = 40        # Domain length in x-direction
-L_y = 40        # Domain lenght in y-direction
+a = 0.5        # Spatial resolution
+L = 40        # Square box size
 sigma_x = 2.5   # Wavefunction shape x-direction
 sigma_y = 2.5   # Wavefunction shape y-direction
-mu_x = 5        # Wavefunction position x-direction
+mu_x = 20        # Wavefunction position x-direction
 mu_y = 20       # Wavefunction position y-direction
-k_x = 99999     # Wave vector x-direction
-k_y = 0         # Wave vector y-direction
+k_x = 0     # Wave vector x-direction
+k_y = 0     # Wave vector y-direction
 # Potential
 xStart =  20
-xEnd = 21
+xEnd = 25
 yStart = 0
 yEnd = 40
-amp = 1         # Potential height
+amp = 0         # Potential height
 
 # Time evolution
 tau = 0.1
 duration = 100
 
-particle1 = Particle(a,L_x,L_y,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y)     # Initialize particle
-particle1.normalize_wavefunction()      # normalize wavefunction so that probability sums up to unity
-particle1.potential(xStart,yEnd,yStart,yEnd,amp)          # Initialize potential
-particle1.timeEvolution(tau,duration)             # Start time evolution of particle
-particle1.plot2D()
+particle = CrankNicolson(a,L,sigma_x,sigma_y,k_x,k_y,mu_x,mu_y)     # Initialize particle
+particle.normalize_wavefunction()      # normalize wavefunction so that probability sums up to unity
+# particle.potential("wall")          # Initialize potential
+particle.timeEvolution(tau,duration)             # Start time evolution of particle
+particle.plot2D()
 # particle1.plot()
